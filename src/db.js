0808ebb8fa280c5a10f5dc1de7130f7c9ee2e3cd
tanzeelalam1989-ux <tanzeelalam1989@gmail.com>
2025-12-11 19:42:46 +0500

    db.js

--- src/db.js
+++ src/db.js
@@ -5,10 +5,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params)
 };
-
